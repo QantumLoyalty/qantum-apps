240d0d21b7dabff0d98df6485d3aa4c87a932cd1
Narendra Bisht <narendrabisht@virtualemployee.com>
2026-07-28 13:45:19 +0530

    updated code

--- lib/core/utils/AppHelper.dart
+++ lib/core/utils/AppHelper.dart
@@ -610,7 +610,7 @@ class AppHelper with LoggingMixin {
       case Flavor.senseOfTaste:
         return const Size(280, 80);
       case Flavor.edp:
-        return const Size(280, 100);
+        return const Size(280, 120);
       case Flavor.mosaic:
         return const Size(100, 90);
       case Flavor.mannumClub:
